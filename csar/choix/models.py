931b35f429ebba203db00874969d476b04a27f76
Thomas Boulier <14161960+tomboulier@users.noreply.github.com>
2023-01-15 16:42:44 +0100

    Création de la base de données

--- csar/choix/models.py
+++ csar/choix/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
-# Create your models here.
+
+class Interne(models.Model):
+    """
+    Classe faisant référence à un interne. Contient les informations basiques
+    d'identité mais également de contact (pour la messagerie).
+
+    Attributs :
+        - nom
+        - prenom
+        - mail
+        - telephone
+    """
+    nom = models.CharField(max_length=200)
+    prenom = models.CharField(max_length=200)
+    mail = models.CharField(max_length=200)
+    telephone = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.prenom + ' ' + self.nom
+
+
+class Stage(models.Model):
+    """
+    Classe faisant référence à un stage. À noter qu'il peut exister plusieurs
+    stages ayant le même nom (par exemple, "Anesthésie pédiatrique premier trimestre").
+
+    Attributs:
+        - interne : l'interne ayant choisi le stage (est égal à NULL tant que pas de choix)
+        - intitule : l'intitulé du stage, exemple "Bloc des urgences deuxième trimestre"
+        - duree : la durée du stage (peut être égale seulement à "3 mois" ou "6 mois")
+    Méthodes :
+        - duree_en_mois : la durée du stage, exprimée en mois (peut être égale seulement à 3 ou 6)
+    """
+    interne = models.ForeignKey(
+        to=Interne,
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+    )
+    intitule = models.CharField(max_length=200)
+    DUREES_STAGES_EN_MOIS = [
+        ('3 mois', 'trimestre'),
+        ('6 mois', 'semestre'),
+    ]
+    duree = models.CharField(
+        max_length=6,
+        choices=DUREES_STAGES_EN_MOIS
+    )
+
+    def duree_en_mois(self):
+        """
+        Retourne la durée, exprimée en mois, sous forme d'entier
+        """
+        if self.duree == '3 mois':
+            return 3
+        if self.duree == '6 mois':
+            return 6
+        raise Exception("l'attribut 'duree' de 'Stage' ne peut être que '3 mois' ou '6 mois'")
+
+    def __str__(self):
+        return self.intitule
