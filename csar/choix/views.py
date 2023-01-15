987ddc7ac4cdd88ba37f2dfb18ce6903dad116d9
Thomas Boulier <14161960+tomboulier@users.noreply.github.com>
2023-01-15 17:17:53 +0100

    Affichage de la liste des stages dans l'index

--- csar/choix/views.py
+++ csar/choix/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Stage
+
+
+def index(request):
+    liste_stages = Stage.objects.all()
+    context = {'liste_stages': liste_stages}
+    return render(request, 'choix/index.html', context=context)
