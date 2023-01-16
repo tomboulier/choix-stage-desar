825ff7c507ad2f049e5e1b13f2ea4482af33e9d5
Thomas Boulier <14161960+tomboulier@users.noreply.github.com>
2023-01-16 21:15:25 +0100

    Redirige la page d'accueil vers l'app

--- csar/csar/urls.py
+++ csar/csar/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import include, path
 
 urlpatterns = [
+    path('', lambda req: redirect('/choix/')),
     path('choix/', include('choix.urls')),
     path('admin/', admin.site.urls),
 ]
