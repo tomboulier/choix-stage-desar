d85a3d8cd3debc7596757508cc777b3cd9bf1046
Thomas Boulier <14161960+tomboulier@users.noreply.github.com>
2023-01-18 19:46:10 +0100

    Ajout de la vue associée à l'UUID d'un interne

--- csar/choix/urls.py
+++ csar/choix/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<str:interne_uuid>/', views.choix_interne, name='choix_interne')
 ]
--- csar/choix/views.py
+++ csar/choix/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.core.exceptions import MultipleObjectsReturned
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 
-from .models import Stage
+from .models import Interne, Stage
 
 
-def index(request):
+def index(request, error_message=None):
     liste_stages = Stage.objects.all().order_by("intitule")
     context = {'liste_stages': liste_stages}
+    if error_message is not None:
+        context['error_message'] = error_message
     return render(request, 'choix/index.html', context=context)
+
+
+def choix_interne(request, interne_uuid):
+    try:
+        interne = get_object_or_404(Interne, uuid=interne_uuid)
+    except MultipleObjectsReturned:  # si plusieurs internes ont le même uuid
+        return index(request, error_message="Erreur: plusieurs internes ont le même identifiant unique")
+    return render(request, 'choix/interne.html', {'interne': interne})
