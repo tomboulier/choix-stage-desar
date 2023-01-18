f7546283f9e76055081072af108d46f3a7728558
Thomas Boulier <14161960+tomboulier@users.noreply.github.com>
2023-01-18 15:25:48 +0100

    Prise en compte de la relation plusieurs-à-plusieurs dans la base de données
    La relation entre "Interne" et "Stage" est de type "plusieurs-à-plusieurs". Ceci est implémenté facilement dans Django (cf. https://docs.djangoproject.com/fr/4.1/topics/db/models/#many-to-one-relationships).
    Cela permettra d'éviter d'avoir à ré-inventer la roue dans le futur pour les recherches dans la base de données.

--- csar/choix/tests.py
+++ csar/choix/tests.py
@@ -1,3 +1,47 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Interne, Stage, Choix
+
+
+class StageModelsTests(TestCase):
+    stage_trimestre = Stage(intitule="stage 3 mois", duree="trimestre")
+    stage_semestre = Stage(intitule="stage 6 mois", duree="semestre")
+
+    def test_duree_en_mois(self):
+        self.assertEqual(self.stage_trimestre.duree_en_mois(), 3)
+        self.assertEqual(self.stage_semestre.duree_en_mois(), 6)
+
+    def test_compte_nombre_postes_disponibles(self):
+        stage = Stage.objects.create(intitule="stage", nombre_postes_ouverts=3)
+        interne1 = Interne.objects.create(prenom="Interne", nom="1")
+        interne2 = Interne.objects.create(prenom="Interne", nom="2")
+
+        # interne 1 et 2 choisissent tous les deux le stage
+        Choix.objects.create(interne=interne1, stage=stage)
+        Choix.objects.create(interne=interne2, stage=stage)
+        self.assertEqual(stage.nombre_postes_disponibles(), 1)
+
+
+class ChoixModelsTests(TestCase):
+    def test_relation_plusieurs_a_plusieurs(self):
+        """
+        On vérifie dans ce jeu de tests qu'on peut bel et avoir plusieurs internes
+        dans un même stage, et qu'un interne peut choisir plusieurs stages.
+        """
+        stage1 = Stage.objects.create(intitule="stage 1")
+        stage2 = Stage.objects.create(intitule="stage 2")
+        interne1 = Interne.objects.create(prenom="Interne", nom="1")
+        interne2 = Interne.objects.create(prenom="Interne", nom="2")
+
+        # l'interne 1 choix le stage 1 et le stage 2
+        interne1.stages.add(stage1)
+        interne1.stages.add(stage2)
+        # on vérifie qu'il y a bien 2 stages associés à l'interne 1
+        self.assertEqual(interne1.stages.all().count(), 2)
+
+        # l'interne 2 choisit aussi le stage 1
+        interne2.stages.add(stage1)
+
+        # on vérifie que les deux internes sont dans le stage 1
+        self.assertIn(interne1, stage1.interne_set.all())
+        self.assertIn(interne2, stage1.interne_set.all())
